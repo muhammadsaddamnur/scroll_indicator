204625f553a12ff15f2709b160013640637b8e93
muhammadsaddamnur <muhammadsaddamnur@gmail.com>
2021-05-04 11:18:28 +0700

    license

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,12 +29,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   ScrollController scrollController = ScrollController();
+  ScrollController scrollController2 = ScrollController();
+  ScrollController scrollController3 = ScrollController();
+
   double pixels = 0.0;
 
   @override
   void initState() {
     scrollController = ScrollController();
-
+    scrollController2 = ScrollController();
+    scrollController3 = ScrollController();
     super.initState();
   }
 
@@ -45,34 +49,111 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 120,
-              width: MediaQuery.of(context).size.width,
-              child: ListView.builder(
-                  controller: scrollController,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 120,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 123,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blue,
+                          child: Text(index.toString()),
+                        ),
+                      );
+                    }),
+              ),
+              ScrollIndicator(
+                scrollController: scrollController,
+                width: 50,
+                height: 5,
+                indicatorWidth: 20,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[300]),
+                indicatorDecoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 120,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  controller: scrollController2,
                   scrollDirection: Axis.horizontal,
-                  itemCount: 123,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.blue,
-                        child: Text(index.toString()),
+                  children: List.generate(100, (index) {
+                    return Center(
+                      child: Text(
+                        'Item $index',
+                        style: Theme.of(context).textTheme.headline5,
                       ),
                     );
                   }),
-            ),
-            ScrollIndicator(
-              scrollController: scrollController,
-              width: 50,
-              height: 5,
-            ),
-          ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ScrollIndicator(
+                scrollController: scrollController2,
+                width: 50,
+                height: 10,
+                indicatorWidth: 20,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(color: Colors.grey[300]),
+                indicatorDecoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 120,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                    controller: scrollController3,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 123,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blue,
+                          child: Text(index.toString()),
+                        ),
+                      );
+                    }),
+              ),
+              ScrollIndicator(
+                scrollController: scrollController3,
+                width: 10,
+                height: 5,
+                indicatorWidth: 20,
+                alignment: Alignment.centerRight,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue),
+                indicatorDecoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/scroll_indicator.dart
+++ lib/scroll_indicator.dart
@@ -3,25 +3,29 @@ library scroll_indicator;
 import 'package:flutter/material.dart';
 
 class ScrollIndicator extends StatefulWidget {
+  ///scrollController listview/gridview
+  ///
   final ScrollController scrollController;
   final double width, height, indicatorWidth;
   final Decoration decoration, indicatorDecoration;
+  final AlignmentGeometry alignment;
   ScrollIndicator(
       {required this.scrollController,
       this.width = 100,
       this.height = 10,
       this.indicatorWidth = 20,
       this.decoration = const BoxDecoration(color: Colors.black26),
-      this.indicatorDecoration = const BoxDecoration(color: Colors.black)});
+      this.indicatorDecoration = const BoxDecoration(color: Colors.black),
+      this.alignment = Alignment.center});
 
   @override
   _ScrollIndicatorState createState() => _ScrollIndicatorState();
 }
 
 class _ScrollIndicatorState extends State<ScrollIndicator> {
-  double pixels = 0.0;
-  double widthContainer = 0.0;
-  double currentPixel = 0.0;
+  double currentPixels = 0.0;
+  double mainContainer = 0.0;
+  double move = 0.0;
 
   @override
   void initState() {
@@ -39,39 +43,33 @@ class _ScrollIndicatorState extends State<ScrollIndicator> {
 
   void _scrollListener() {
     setState(() {
-      pixels = widget.scrollController.position.pixels;
-      widthContainer =
+      currentPixels = widget.scrollController.position.pixels;
+      mainContainer =
           widget.scrollController.position.maxScrollExtent / widget.width;
-      currentPixel = (pixels / widthContainer);
-
-      // debugPrint(
-      //     'current ' + widget.scrollController.position.pixels.toString());
-      // debugPrint(
-      //     'max ' + widget.scrollController.position.maxScrollExtent.toString());
-      // debugPrint('widthContainer ' +
-      //     (widget.scrollController.position.maxScrollExtent / width)
-      //         .toString());
-      // debugPrint('currentPixel ' + (pixels / widthContainer).toString());
+      move = (currentPixels / mainContainer);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: widget.height,
-      width: widget.width + widget.indicatorWidth,
-      decoration: widget.decoration,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Positioned(
-              left: currentPixel,
-              child: Container(
-                height: widget.height,
-                width: widget.indicatorWidth,
-                decoration: widget.indicatorDecoration,
-              )),
-        ],
+    return Align(
+      alignment: widget.alignment,
+      child: Container(
+        height: widget.height,
+        width: widget.width + widget.indicatorWidth,
+        decoration: widget.decoration,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+                left: move,
+                child: Container(
+                  height: widget.height,
+                  width: widget.indicatorWidth,
+                  decoration: widget.indicatorDecoration,
+                )),
+          ],
+        ),
       ),
     );
   }
